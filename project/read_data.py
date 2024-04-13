8daa55c231361b053ee583090b11fc997033fd92
Souhail Abdellaoui <souhail.abdellaoui@student-cs.fr>
2024-04-13 21:32:46 +0200

    on verra

--- project/read_data.py
+++ project/read_data.py
@@ -44,34 +44,34 @@ def LoadDataSetMultiCoil(load_dir, **kwargs):
         file_name = os.path.join(load_dir, k_index, "cine_sax.mat")
         with h5py.File(file_name, 'r') as f:
             newvalue = f['kspace_full']
-            fullmulti = newvalue["real"] + 1j * newvalue["imag"]
-            [nframe, nslice, ncoil, ny, nx] = fullmulti.shape
-            
-            fullmulti = T.to_tensor(fullmulti)
+        fullmulti = newvalue["real"] + 1j * newvalue["imag"]
+        [nframe, nslice, ncoil, ny, nx] = fullmulti.shape
+        
+        fullmulti = T.to_tensor(fullmulti)
 
-            complex_image = fastmri.ifft2c(fullmulti)
-            image_rss = fastmri.rss(complex_image, dim=2)
-            image_rss = image_rss.numpy()
-            image = image_rss[:,:,:,:,0] + 1j*image_rss[:,:,:,:,1]
-            
+        complex_image = fastmri.ifft2c(fullmulti)
+        image_rss = fastmri.rss(complex_image, dim=2)
+        image_rss = image_rss.numpy()
+        image = image_rss[:,:,:,:,0] + 1j*image_rss[:,:,:,:,1]
+        
 
-            # Calculate padding amounts
-            pad_y = (desired_ny - ny) if ny < desired_ny else 0
-            pad_x = (desired_nx - nx) if nx < desired_nx else 0
+        # Calculate padding amounts
+        pad_y = (desired_ny - ny) if ny < desired_ny else 0
+        pad_x = (desired_nx - nx) if nx < desired_nx else 0
 
-            # Padding to be applied on both sides of the dimensions
-            pad_y_before = pad_y // 2
-            pad_y_after = pad_y - pad_y_before
-            pad_x_before = pad_x // 2
-            pad_x_after = pad_x - pad_x_before
+        # Padding to be applied on both sides of the dimensions
+        pad_y_before = pad_y // 2
+        pad_y_after = pad_y - pad_y_before
+        pad_x_before = pad_x // 2
+        pad_x_after = pad_x - pad_x_before
 
-            # Apply padding
-            image = np.pad(
-                image, pad_width=((0, 0), (0, 0), (pad_y_before, pad_y_after), (pad_x_before, pad_x_after)), mode='constant', constant_values=(0, 0)
-            )
+        # Apply padding
+        image = np.pad(
+            image, pad_width=((0, 0), (0, 0), (pad_y_before, pad_y_after), (pad_x_before, pad_x_after)), mode='constant', constant_values=(0, 0)
+        )
 
-            image_list = image.reshape(nframe * nslice, 512, 512)
-            images_list.append(image_list)
+        image_list = image.reshape(nframe * nslice, 512, 512)
+        images_list.append(image_list)
 
 
     # Concatenate all images
@@ -129,7 +129,7 @@ def get_fs_multicoil(data_dir, phase = 'train'):
     [Number of images x height x width]
     """
 
-    data_fs = LoadDataSetMultiCoil(data_dir)
+    data_fs = np.load('/usr/users/cei2023_2024_inserm_nir_irm/abdellaoui_sou/PFE/data.npy')
 
     return data_fs
 
