bedab5565cfa38f518db5481a16aa70bcc0fe407
Souhail Abdellaoui <souhail.abdellaoui@student-cs.fr>
2024-04-13 22:57:08 +0200

    on verra

--- project/utils/fdb.py
+++ project/utils/fdb.py
@@ -93,7 +93,7 @@ class DiffusionBridge:
         img = x_0[:, [0]] + x_0[:, [1]] * 1j
         img = self.fft2c(img)
         img_0 = th.clone(img)
-        img_t_minus_1 = None
+        img_t_minus_1 = img_0
 
         for i in range(n):
             x = np.random.randint(dim)
