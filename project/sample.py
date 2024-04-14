284258d0520ab3b48244b14827fc2217f591e123
Souhail Abdellaoui <souhail.abdellaoui@student-cs.fr>
2024-04-15 00:34:23 +0200

    on verra

--- project/sample.py
+++ project/sample.py
@@ -94,7 +94,7 @@ def main():
         vis_1 = np.abs(coarse_np[0,-1,0,:,:] + coarse_np[0,-1,1,:,:]*1j)
         logger.log(str(vis_1.shape))
         savedict = {}
-        savedict[image] = vis_1
+        savedict["image"] = vis_1
         filepath = os.path.join(args.save_path, "image" + '_' + str(index) + '.png'), vis_1/vis_1.max()
         hdf5storage.savemat(filepath, savedict)
         #imageio.imsave(os.path.join(args.save_path, "image" + '_' + str(index) + '.png'), vis_1/vis_1.max())
