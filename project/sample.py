409be645946be3628a395aa118b4263f87b6fca3
Souhail Abdellaoui <souhail.abdellaoui@student-cs.fr>
2024-04-14 18:27:41 +0200

    Sample step

--- project/sample.py
+++ project/sample.py
@@ -27,26 +27,39 @@ def main():
     logger.configure(dir=args.save_path)
 
     logger.log("creating model and diffusion...")
+    logger.log("STEP 1")
     model, diffusion = create_model_and_diffusion(
         **args_to_dict(args, model_and_diffusion_defaults().keys())
     )
+    logger.log("STEP 2")
     model.load_state_dict(
         dist_util.load_state_dict(args.model_path, map_location="cpu")
     )
+    logger.log("STEP 3")
     model.to(dist_util.dev())
+    logger.log("STEP 4")
 
     model.eval()
+    logger.log("STEP 5")
     for index in range(len(images)):
+        logger.log("In loop...")
+        logger.log("Loop : STEP 1")
         mask = th.from_numpy(masks[np.newaxis,np.newaxis,index]).cuda()
+        logger.log("Loop : STEP 2")
         mask = th.cat([mask,mask],1)
+        logger.log("Loop : STEP 3")
 
         kspace = load_data(images, index, args.batch_size, args.data_type)
 
+        logger.log("Loop : STEP 4")
+
         if args.data_type == "multicoil":
+            logger.log("Loop : STEP 5")
             coil_map = th.from_numpy(coil_maps[np.newaxis, index, :, :, :]).cuda()
         else:
             coil_map = None
 
+        logger.log("Loop : STEP 6")
         start_time = time.time()
         print("Index:", index)
         sample = diffusion.p_sample_loop_condition(
@@ -58,19 +71,22 @@ def main():
         )[-1]
         total_time = time.time() - start_time
         print("total time: " + str(total_time))
+        logger.log("Loop : STEP 7")
         samples = []
         coarse = []
         samples.append(sample)
         optimizerG = th.optim.Adam(model.parameters(), lr=1e-4)
-
+        logger.log("Loop : STEP 8")
         samples = th.cat(samples)
         coarse.append(samples.contiguous())
         coarse = th.stack(coarse)
         coarse_np = coarse.cpu().data.numpy()
+        logger.log("Loop : STEP 9")
         np.save(os.path.join(args.save_path, "coarse" + str(index)  + ".npy"), coarse_np)
-
+        logger.log("Loop : STEP 10")
         vis_1 = np.abs(coarse_np[0,-1,0,:,:] + coarse_np[0,-1,1,:,:]*1j)
         imageio.imsave(os.path.join(args.save_path, "image" + '_' + str(index) + '.png'), vis_1/vis_1.max())
+        logger.log("Loop : STEP Finale")
         print(args.save_path)
 
 
